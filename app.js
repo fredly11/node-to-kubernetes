eb25b2d6dd9fa269505b9520c028ea7f23c67145
unknown <buechelewd@gmail.com>
2021-07-01 11:23:54 -0600

    Edit port

--- app.js
+++ app.js
@@ -1,6 +1,6 @@
 // Load the http module to create an http server.
 var http = require('http');
-let port = 3000
+let PORT = 4000
 
 // Configure our HTTP server to respond with Hello World to all requests.
 var server = http.createServer(function (request, response) {
@@ -8,8 +8,8 @@ var server = http.createServer(function (request, response) {
   response.end("Hello World blargyblarg\n");
 });
 
-// Listen on port 3000, IP defaults to 127.0.0.1
-server.listen({port});
+// Listen on PORT 3000, IP defaults to 127.0.0.1
+server.listen({PORT});
 
 // Put a friendly message on the terminal
-console.log(`Server running at http://127.0.0.1:${port}/`);
+console.log(`Server running at http://127.0.0.1:${PORT}/`);
